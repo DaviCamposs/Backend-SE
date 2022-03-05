18fe07057ca08b62be8c82b3ae6f77523005f50b
Davi Campos <davi.campos689@gmail.com>
2022-03-05 10:42:08 -0300

    feat: add modulo model

---
+++ models/modulo.js
@@ -0,0 +1,14 @@
+const { Schema , model } = require("mongoose")
+
+const modulo = new Schema({
+    nome: {
+        type: String,
+        required: true
+    },
+    descricao: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = model('Modulo', module)
